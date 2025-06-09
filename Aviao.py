f068e5d26928231a42c3025f0a84214bd52b8f1f
MatheusHMF-fax <82951538+MatheusHMF-fax@users.noreply.github.com>
2025-06-09 11:49:13 -0300

    1.0

---
+++ Aviao.py
@@ -0,0 +1,10 @@
+import sqlite3
+import pandas as pd
+import streamlit as st
+from datetime import datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("resumo_anual_2025.csv", delimiter=';', encoding='latin')
+
+print(df)
